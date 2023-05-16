adc1d78162f59fa8715d9e4a71f9ff3bf87225d6
Santiago A <santiagoaloi@gmail.com>
2023-05-16 08:08:56 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <VApp :theme="currentTheme">
+    <!-- handles layouts only -->
+    <RouterView />
+  </VApp>
+</template>
+
+<script setup>
+import { useTailwindTheme } from '@C/tailwind-theme'
+const { currentTheme } = toRefs(useAppStore())
+useTailwindTheme(currentTheme)
+</script>
